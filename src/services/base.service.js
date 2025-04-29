457cbfea518143625ed3033927ecdf504f8dcc28
Deepak Singh <deepaky954000@gmail.com>
2025-04-29 17:34:34 +0530

    base controller and base service added

---
+++ src/services/base.service.js
@@ -0,0 +1,32 @@
+class Service {
+  static Model = null;
+
+  static async get(id, filters) {
+    if (!id) {
+      return await this.Model.find(filters);
+    }
+    return await this.Model.findDocById(id);
+  }
+
+  static async create(data) {
+    const createdDoc = await this.Model.create(data);
+    return createdDoc;
+  }
+
+  static async update(id, data) {
+    const doc = await this.Model.findDocById(id);
+
+    doc.updateFields(data);
+    await doc.save();
+
+    return doc;
+  }
+
+  static async deleteDoc(id) {
+    const doc = await this.Model.findDocById(id);
+
+    //TODO: Delete functionality has to be implemented;
+  }
+}
+
+export default Service;
