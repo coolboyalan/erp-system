ac9823632499543e63d4b1f490e6cc7aa3a5bc34
Deepak Singh <deepaky954000@gmail.com>
2025-05-03 14:20:13 +0530

    added password generation for employee

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,8 +1,15 @@
 import User from "#models/user";
 import BaseService from "#services/base";
+import randomPassword from "#utils/password";
 
 class UserService extends BaseService {
   static Model = User;
+
+  static async create(data) {
+    const { password } = data;
+    password = password ?? randomPassword(8);
+    return await super.create(data);
+  }
 }
 
 export default UserService;
