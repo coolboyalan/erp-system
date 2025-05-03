0e916b7df693baeb4e6b1f3385e0a4eaf3b131c4
Deepak Singh <deepaky954000@gmail.com>
2025-05-03 18:51:10 +0530

    bin get all api modified

--- src/services/bin.service.js
+++ src/services/bin.service.js
@@ -3,6 +3,16 @@ import BaseService from "#services/base";
 
 class BinService extends BaseService {
   static Model = Bin;
+
+  static async get(id, filters) {
+    if (!id) {
+      const options = {
+        fields: ["id", "name"],
+      };
+      return await this.Model.find(filters, options);
+    }
+    return this.Model.findDocById(id);
+  }
 }
 
 export default BinService;
