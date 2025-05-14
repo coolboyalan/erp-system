7e9eadacaec9e592b021404ccd099bbccd30debf
Deepak Singh <deepaky954000@gmail.com>
2025-05-14 13:16:41 +0530

    added category delete

--- src/services/productCategory.service.js
+++ src/services/productCategory.service.js
@@ -1,8 +1,15 @@
 import ProductCategory from "#models/productCategory";
+import { session } from "#middlewares/requestSession";
 import BaseService from "#services/base";
 
 class ProductCategoryService extends BaseService {
   static Model = ProductCategory;
+
+  static async deleteDoc(id) {
+    const doc = await this.Model.findDocById(id);
+
+    await doc.destroy({ force: true, transaction: session.get("transaction") });
+  }
 }
 
 export default ProductCategoryService;
