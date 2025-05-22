0a43bd18379da0ad0641fd5a011df517640f2b01
Deepak Singh <deepaky954000@gmail.com>
2025-05-22 10:46:07 +0530

    added warehouse total API

--- src/controllers/warehouse.controller.js
+++ src/controllers/warehouse.controller.js
@@ -1,8 +1,15 @@
 import WarehouseService from "#services/warehouse";
 import BaseController from "#controllers/base";
+import { sendResponse } from "#utils/response";
 
 class WarehouseController extends BaseController {
   static Service = WarehouseService;
+
+  static async getTotal(req, res, next) {
+    const { warehouseId } = req.query;
+    const data = await this.Service.getTotalValue(warehouseId);
+    sendResponse(httpStatus.OK, res, data);
+  }
 }
 
 export default WarehouseController;
--- src/routes/warehouse.route.js
+++ src/routes/warehouse.route.js
@@ -5,13 +5,19 @@ import { authentication } from "#middlewares/authentication";
 
 const router = express.Router();
 
-// router.use(authentication);
+router.use(authentication);
+
+router
+  .route("/total")
+  .get(asyncHandler(WarehouseController.get.bind(WarehouseController)));
 
 router
   .route("/:id?")
   .get(asyncHandler(WarehouseController.get.bind(WarehouseController)))
   .post(asyncHandler(WarehouseController.create.bind(WarehouseController)))
   .put(asyncHandler(WarehouseController.update.bind(WarehouseController)))
-  .delete(asyncHandler(WarehouseController.deleteDoc.bind(WarehouseController)));
+  .delete(
+    asyncHandler(WarehouseController.deleteDoc.bind(WarehouseController)),
+  );
 
 export default router;
